9aad75690c856711d38b8341b7f88c12b3574878
arcm84 <124362565+arcm84@users.noreply.github.com>
2025-12-12 18:29:53 +0000

    Finished! Basic etch a sketch complete.

--- js/index.js
+++ js/index.js
@@ -2,14 +2,35 @@
 
 console.log("test can you hear me?")
 
-const container1 = document.querySelector("#container1");
+const masterContainer = document.querySelector("#master-container");
 
-for (let step = 1; step < 5; step++) {
-  const div = document.createElement("div");
+for (let stepc = 1; stepc < 50; stepc++) {
+  const container = document.createElement("div");
 
-  div.setAttribute("style", "color: blue; background: grey;");
-  div.setAttribute("id", `div${step}`);
-  div.textContent = `This is div${step}`;
+  container.setAttribute("id", `container${stepc}`);
 
-  container1.appendChild(div);
+  for (let step = 1; step < 50; step++) {
+    const div = document.createElement("div");
+
+    div.setAttribute("id", `div${stepc}_${step}`);
+    div.className = "pixel-default";
+
+    /* to get the toggle effect to work, we need to use
+    the onMouseEnter property. When the mouse enters the
+    div, it changes from one style to another. When the 
+    event happens again, it reverts back. This means there
+    needs to be a `blank` and `filled` div style to switch
+    between in the .css file.*/
+
+    div.onmouseenter = function() {
+      if (div.className == "pixel-default") {
+        div.className = "pixel-highlight";
+      } else {
+        div.className = "pixel-default";
+      };
+    };
+
+    container.appendChild(div);
+  };
+  masterContainer.appendChild(container);
 };
